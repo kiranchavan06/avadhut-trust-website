e5fe2e8d9daeeca6e27a512812a3776f24924c15
avadhut-trust <raaammm213@gmail.com>
2026-05-15 07:22:16 +0530

    Add Firebase database

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCm7G8W2EM2B9nNvujP8SQQht7HHkQYqLA",
+  authDomain: "avadhut-trust.firebaseapp.com",
+  projectId: "avadhut-trust",
+  storageBucket: "avadhut-trust.firebasestorage.app",
+  messagingSenderId: "291155545994",
+  appId: "1:291155545994:web:2aaf2ad05bc1c716630273"
+};
